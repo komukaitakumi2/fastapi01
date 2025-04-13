e09beaf7f3a6033bbac0f47dc002195a5300750d
komukaitakumi2 <komukaitakumi@komukaitakuhitsujinoMacBook-Air.local>
2025-04-13 15:45:11 +0900

    かなり動かせたよ！

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI ##FastAPIの本体をインポート
+# main.py
+from fastapi import FastAPI
 
+app = FastAPI()
 
-app = FastAPI()##アプリケーションを作成
-
-@app.get("/")##/にアクセスした時の処理(GETリクエスト)を定義
-
-def read_root():##エンドポイントに対応する関数(処理の中身)
-    return {"message": "Hello, FastAPI!"}##クライアントに返すJSONレスポンス
+@app.get("/")
+def read_root():
+    return {"message": "Hello from my local repo!Loveff"}
